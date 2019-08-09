f1fd182562afe255550bfb82cded5704f82e8280
beyond <921790476>
2019-08-09 14:27:40 +0800

    init

---
+++ meite-shop-service-impl/meite-shop-service-weixin/src/main/java/com/mayikt/weixin/service/impl/WeiXinAppServiceImpl.java
@@ -0,0 +1,12 @@
+package com.mayikt.weixin.service.impl;
+
+import com.mayikt.weixin.entity.AppEntity;
+import com.mayikt.weixin.service.WeiXinAppService;
+
+public class WeiXinAppServiceImpl implements WeiXinAppService {
+
+    @Override
+    public AppEntity getApp() {
+        return null;
+    }
+}
